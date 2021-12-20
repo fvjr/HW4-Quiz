ee839d93c70e41a0cf6e2eda11f49af4d5575427
Fernando Vasquez <vasquezjrf@gmail.com>
2021-12-20 18:34:51 -0500

    updated html answer boxes, css for answer boxes, and scripts for questions/answers

--- Assets/script.js
+++ Assets/script.js
@@ -1,103 +1,114 @@
 //Access to start button
 var startButton = document.getElementById("start-button");
 //Access value of time remaining 
-var remainingTime = document.getElementById("time");
+var time = document.getElementById("time");
 //Initial amount of time for quiz 
 var timeLeft = 60;
 //Access area where questions will be displayed
-var questionsDisplay = document.getElementById("quiz-questions");
+var questionDisplay = document.getElementById("question");
 //Access area where answers will be displayed
-var answersDisplay = document.querySelectorAll(".answers")
+var answerChoices = document.querySelectorAll(".answer-text")
 //Access elements to remove them from screen
 var removeIntro = document.getElementById("intro");
 var removeInstructions = document.querySelector(".quiz-instructions");
-//Access answer buttoms
-var answerButtons = document.querySelector(".answer-buttons");
+//saves user name
+// var userName = ; 
+//saves score
+var score = timeLeft;
+//Text for incorrect answer
+var answerValidator = document.getElementById("results");
 
-//Quiz questions
+var currentQuestion = {};
+
+//Quiz questions object
 var questions = [
-   {
- question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
- answers: {
- a: '===',
- b: '==', 
- c: '<=>',
- d: '(=)',
-},
-correctAnswer: "a"
-},
-{  question: 'What are the three ways to declare a variable in Javascript?',
-answers: {
-a: 'var, let, set-variable',
-b: 'let, set, scope', 
-c: 'var, let, const',
-d: 'scope, set-variable, for',
-},
-correctAnswer: "c"
-},
-{  question: 'What is the proper format to set an array?',
-answers: {
-a: 'set array = {}',
-b: 'for array = []', 
-c: 'var array = {}',
-d: 'var array = []',
-},
-correctAnswer: "d"
-},
-{  question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
-answers: {
-a: '===',
-b: '==', 
-c: '<=>',
-d: '(=)',
-},
-correctAnswer: "a"
-},
-{  question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
-answers: {
-a: '===',
-b: '==', 
-c: '<=>',
-d: '(=)',
-},
-correctAnswer: "a"
-}];
-
-//Quiz answers
-
+  {
+    question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
+    choices: ['===', '==', '<=>', '(=)'],
+    correctAnswer: "a",
+  },
+  {
+    question: 'Which of the following Javascript operators would you use to check strict equality between two different values? --2',
+    choices: ['===', '==', '<=>', '(=)'],
+    correctAnswer: "a",
+  },
+  {
+    question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
+    choices: ['===', '==', '<=>', '(=)'],
+    correctAnswer: "a",
+  },
+  {
+    question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
+    choices: ['===', '==', '<=>', '(=)'],
+    correctAnswer: "a",
+  },
+  {
+    question: 'Which of the following Javascript operators would you use to check strict equality between two different values?',
+    choices: ['===', '==', '<=>', '(=)'],
+    correctAnswer: "a",
+  }];
 
 
 // Function for start button
-startButton.addEventListener('click', function() {
-  console.log("button works!");
-  //starts timer 
-  var timerInterval = setInterval(function() {
+startButton.addEventListener('click', function () {
+  //removes start button
+  startButton.remove();
+  //removes quiz intro
+  removeIntro.remove();
+  //removes quiz instructions
+  removeInstructions.remove();
+  var quizBox = document.querySelector(".quiz-box");
+  quizBox.classList.remove("hide");
+  //starts timer starting at 60s (timeLeft)
+  var timer = setInterval(function () {
     timeLeft--;
     time.textContent = timeLeft + " seconds remaining, hurry!";
-    if(timeLeft === 0) {
+    if (timeLeft === 0) {
+      endGame();
       //Stops timer when it hits 0 
-      clearInterval(timerInterval);
+      clearInterval(timer);
+      time.setAttribute('style', 'color:red');
       time.textContent = "You're out of time!";
     }
-    //removes introduction and instructions on timer start
-removeIntro.remove();
-removeInstructions.remove();
-startButton.remove();
-// questionsDisplay.add();
-// answersDisplay.add();
+  }, 500);
+callQuestion();
+});
 
-
-  }, 10);
-
-for (var i = 0; i < questions.length; index++) {
-  const element = questions[i];
+function callQuestion() {
+  questionDisplay.textContent = questions[q].question;
+  var answerContainer = document.querySelector(".answers-container");
+  answerContainer.innerHTML = "";
+  questions[q].choices.forEach(function(choice){
+  var answerBox = document.createElement("div");
+  answerBox.setAttribute("class", "answers-box");
+  answerBox.textContent = choice;
+  answerBox.dataset.choice = choice;
+  answerBox.onclick = checkAnswer;
+  answerContainer.appendChild(answerBox);
+  });
+}
+function checkAnswer(event){
+  var choice =event.target.dataset.choice; 
+  if (choice === questions[q].correctAnswer) {
+    console.log("Correct!");
+  } else {
+    console.log("Wrong!");
+  } 
+  q ++;
+  if (questions.length === q) {
+    console.log("End game")
+  } else {
+    callQuestion();
+  }
   
-}});
+}
+function endGame() {
+  if (q.length = 5 || timeLeft === 0)
+  alert("Game over"); 
+}
 
-//Add buttons when game starts
+var q = 0
 
-
-// // Function for selection of questions
-
-// //Function to save name and score
-
+// replace console logs with functions
+// In local storage i want to make an object for each score with initial and score, push those into an array of objects, stringify that array into local storage and then parse it out of local storage
+// stingify data in, parse out
